8427ac76dbf64079e89c3e8a621d4a503bc92b6b
KM Kollman <k.m.kollman@gmail.com>
2018-02-19 22:09:58 +0100

    - prepare tablet styles

--- js/script.js
+++ js/script.js
@@ -15,10 +15,14 @@ document.addEventListener("DOMContentLoaded", function() {
   //add on-scroll class to topbar
 
   const toggleFloatingBar = function() {
-    console.log('dupa')
-    window.pageYOffset > 50
-    ? document.querySelector('.flexo-topbar').classList.add('floating')
-      : document.querySelector('.flexo-topbar').classList.remove('floating')
+    if(window.pageYOffset > 50) {
+      document.querySelector('.flexo-topbar').classList.add('floating');
+      document.querySelector('.hamburger-icon').classList.add('floating')
+    } else {
+      document.querySelector('.flexo-topbar').classList.remove('floating')
+      document.querySelector('.hamburger-icon').classList.remove('floating')
+    }
+
   }
 
   document.addEventListener('scroll', () => toggleFloatingBar())
